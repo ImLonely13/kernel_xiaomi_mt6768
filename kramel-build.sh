d9aa344a78b5b524f7de9239959c18602aa23273
Panchajanya1999 <rsk52959@gmail.com>
2022-06-26 06:01:38 +0000

    CI: Init github actions CI Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com> Signed-off-by: Himemorii <himemori@mail.com> Change-Id: Idcf2c96c434a39b85ee4278bc38be8781eb11b45 Change-Id: If912521b330a3efbf30acdbc8b38d039f519f34e Signed-off-by: ImLonely13 <gabutuhaku@gmail.com>

---
+++ kramel-build.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/bash
+# Written by: cyberknight777
+# YAKB v1.0
+# Copyright (c) 2022-2023 Cyber Knight <cyberknight755@gmail.com>
+#
+#			GNU GENERAL PUBLIC LICENSE
+#			 Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
+# Some Placeholders: [!] [*] [✓] [✗]
+
+# A function to send message(s) via Telegram's BOT api.
+tg() {
+    curl -sX POST https://api.telegram.org/bot"${TG_TOKEN}"/sendMessage \
+        -d chat_id="-1001754559150" \
+        -d parse_mode=html \
+        -d disable_web_page_preview=true \
+        -d text="$1"
+}
+
+tgs() {
+    SHA1=$(sha1sum "$1" | cut -d' ' -f1)
+    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"${TG_TOKEN}"/sendDocument \
+        -F "chat_id=-1001754559150" \
+        -F "parse_mode=Markdown" \
+        -F "caption=$2 | *SHA1*: \`$SHA1\`"
+}
+
+# Default defconfig to use for builds.
+CONFIG="merlin_defconfig"
+
+# Default directory where kernel is located in.
+KDIR=$(pwd)
+
+# User and Host name
+export KBUILD_BUILD_USER=ItsProf
+export KBUILD_BUILD_HOST=github.com
+
+# Number of jobs to run.
+PROCS=$(nproc --all)
+
+Ai1() {
+    
+    echo -e "\n\e[1;93m[*] Cloning Toolchain! \e[0m"
+    git clone https://github.com/kenhv/gcc-arm64 --depth=1 -b master "${KDIR}"/gcc32
+    git clone https://github.com/kenhv/gcc-arm --depth=1 -b master "${KDIR}"/gcc64
+    git clone https://github.com/ImLonely13/AnyKernel3 -b merlin "${KDIR}"/anykernel3
+    echo -e "\n\e[1;32m[✓] Cloning Done! \e[0m"
+
+    LLD_VER=$("${KDIR}"/gcc64/bin/aarch64-elf-ld.lld -v | head -n1 | sed 's/(compatible with [^)]*)//' |
+            head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+    KBUILD_COMPILER_STRING=$("${KDIR}"/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
+    export KBUILD_COMPILER_STRING
+    export PATH="${KDIR}"/gcc32/bin:"${KDIR}"/gcc64/bin:/usr/bin/:${PATH}
+    MAKE+=(
+        ARCH=arm64
+        O=out
+        CC=aarch64-elf-gcc
+        LD=aarch64-elf-ld.lld
+        CROSS_COMPILE=aarch64-elf-
+        CROSS_COMPILE_ARM32=arm-eabi-
+        AR=llvm-ar
+        NM=llvm-nm
+        OBJDUMP=llvm-objdump
+        OBJCOPY=llvm-objcopy
+        OBJSIZE=llvm-objsize
+        STRIP=llvm-strip
+        HOSTAR=llvm-ar
+        HOSTCC=gcc
+        HOSTCXX=aarch64-elf-g++
+    )
+
+    export KBUILD_BUILD_VERSION=$GITHUB_RUN_NUMBER
+    export KBUILD_BUILD_HOST=$HOST
+    export KBUILD_BUILD_USER=$BUILDER
+    zipn=ProjectRandom-kernel-${CODENAME}
+
+
+tg "
+<b>Device</b>: <code>${DEVICE}</code>
+<b>Kernel Version</b>: <code>$(make kernelversion 2>/dev/null)</code>
+<b>Date</b>: <code>$(date)</code>
+<b>Zip Name</b>: <code>${zipn}</code>
+<b>Compiler</b>: <code>${KBUILD_COMPILER_STRING}</code>
+<b>Linker</b>: <code>${LLD_VER}</code>
+"
+
+
+    echo -e "\n\e[1;93m[*] Building Kernel! \e[0m"
+    BUILD_START=$(date +"%s")
+    time make -j"$PROCS" "${MAKE[@]}" Image.gz-dtb dtbo.img 2>&1 | tee log.txt
+    BUILD_END=$(date +"%s")
+    DIFF=$((BUILD_END - BUILD_START))
+    if [ -f "${KDIR}/out/arch/arm64/boot/Image.gz-dtb" ]; then
+            tg "<b>Kernel Built after $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)</b>"
+        echo -e "\n\e[1;32m[✓] Kernel built after $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! \e[0m"
+    else
+            tgs "log.txt" "*Build failed*"
+        echo -e "\n\e[1;31m[✗] Build Failed! \e[0m"
+        exit 1
+    fi
+
+    echo -e "\n\e[1;93m[*] Building DTBS! \e[0m"
+    time make -j"$PROCS" "${MAKE[@]}" dtbs dtbo.img
+    echo -e "\n\e[1;32m[✓] Built DTBS! \e[0m"
+
+        tg "<b>Building zip!</b>"
+    echo -e "\n\e[1;93m[*] Building zip! \e[0m"
+    cat "${KDIR}"/out/arch/arm64/boot/dts/mediatek/mt6768.dtb "${KDIR}"/anykernel3/dtb
+    mv "${KDIR}"/out/arch/arm64/boot/dtbo.img "${KDIR}"/anykernel3
+    mv "${KDIR}"/out/arch/arm64/boot/Image.gz-dtb "${KDIR}"/anykernel3
+    cd "${KDIR}"/anykernel3 || exit 1
+    zip -r9 "$zipn".zip . -x ".git*" -x "README.md" -x "LICENSE" -x "*.zip"
+    echo -e "\n\e[1;32m[✓] Built zip! \e[0m"
+        tgs "${zipn}.zip"
+}
+Ai1
